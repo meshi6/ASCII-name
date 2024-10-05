19b4e7bbc6d24120837f654bc0a17762719c63b7
meshi6 <mercedesm@gmail.com>
2024-10-05 17:27:08 -0400

    initials

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello CodeSandbox!")
+#Paris Madanire
+#I play tennis
+
+print('M   M')
+print('MM MM')
+print('MM MM')
+print('  M')
+print('M   M')
+print('M   M')
+print('M   M')
+print('     ')
+print('M   M')
+print('MM MM')
+print('MM MM')
+print('  M')
+print('M   M')
+print('M   M')
+print('M   M')
